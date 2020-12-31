177d200ece2ebeb2df5920edb198651d0fda46b0
Ly Vireakdara <lyvireakdara@gmail.com>
2020-12-31 10:29:38 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text("Ask Me Anything"),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: myWish(),
+          backgroundColor: Colors.blue,
+        ),
       ),
     );
+
+class myWish extends StatefulWidget {
+  @override
+  _myWishState createState() => _myWishState();
+}
+
+class _myWishState extends State<myWish> {
+  int ballImg = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  ballImg = Random().nextInt(5) + 1;
+                });
+                print("Button Pressed");
+              },
+              child: Image.asset("images/ball$ballImg.png"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
